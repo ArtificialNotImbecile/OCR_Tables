351c33b1d07b3842e9e1c0f70d390647628a5d7d
Luo <rowyoung@icloud.com>
2018-08-10 20:11:58 +0800

    first commit

---
+++ baidu.py
@@ -0,0 +1,139 @@
+from skimage import filters, segmentation, io, feature
+from skimage.measure import label, regionprops
+from skimage.color import label2rgb
+from scipy import ndimage
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import sys, os
+from matplotlib.pyplot import imread
+from scipy.misc import imsave
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np 
+import pandas as pd
+from aip import AipOcr
+
+
+
+
+
+class Image2Csv_CL:
+    """
+    Convert image of table with CL(clear line) to csv file
+    """
+    APP_ID = '11655489'
+    API_KEY = 'UWRvLnevXSiXVUZA52wic0FN'
+    SECRET_KEY = 'R37MCzrwZgWNV8u9ghz53mctuTO5tIF2'
+    baiduclient = AipOcr(APP_ID, API_KEY, SECRET_KEY)
+    options = {}
+    options["language_type"] = "CHN_ENG"
+    #options["detect_direction"] = "true"
+    #options["detect_language"] = "true"
+    #options["probability"] = "true"
+
+    def __init__(self, image_file, hight_resolution=True):
+        im = imread(image_file)[:,:,0]
+        self.image = im
+        self.image_dir = image_file
+        val  = filters.gaussian(im)
+        mask = im < val
+        clean_border = segmentation.clear_border(mask)
+        if hight_resolution:
+            self.row_vals = list([sum(r) for r in 1-im  ])
+            self.col_vals = list([sum(r) for r in 1-im.T])
+        else:
+            self.row_vals = list([sum(r) for r in clean_border  ])
+            self.col_vals = list([sum(c) for c in clean_border.T])
+
+    def determine_spike_position(self, values):
+        MAX = np.mean(sorted(values)[-2:])
+        # This should be more flexible, consider 1-sigma, two-sigma and three-sigma. Then decide which one is the best.
+        # For example, 2-sigma we have 2 position and 3-sigma we hvae 4 position, and 3-sigma have 100 position. 
+        # Actually, if we want, say 
+        MAX_95_CI_L = MAX - 4*np.sqrt(np.var(values))
+        rough_position = np.where((values > MAX_95_CI_L)==True)[0]
+        fine_position = [rough_position[0]]
+        thd = np.mean(rough_position[1:]-rough_position[:-1])
+        for first, second in zip(rough_position[:-1],rough_position[1:]):
+            if  second - first > 15:
+                fine_position.append(second)
+        return fine_position
+
+    def crop_image(self):
+        # Crop images along col first
+        row_position, col_position = self.determine_spike_position(self.row_vals), self.determine_spike_position(self.col_vals)
+        #row_position.append() 0 position and end position
+        if row_position[0] > 80:
+            row_position.append(self.image.shape[0])
+            row_position.insert(0,0)
+        if col_position[0] > 120:
+            col_position.append(self.image.shape[1])
+            col_position.insert(0,0)
+        self.table_shape = len(row_position)-1,len(col_position)-1,
+        vertival_slices = []
+        for first, second in zip(col_position[:-1],col_position[1:]):
+            vertival_slices.append(self.image[:,first:second])
+        # Crop vertical slices into chunks along row
+        cropped = []
+        for vertival_slice in vertival_slices:
+            for first, second in zip(row_position[:-1],row_position[1:]):
+                cropped.append(vertival_slice[first:second,:])
+        return cropped
+        #return np.array(cropped).reshape(len(col_position)-1,len(row_position)-1).T
+        
+    def image2words(self, image_array_2d):
+        s, encoded_image = cv2.imencode('.png', np.uint8(image_array_2d*255))
+        content = encoded_image.tobytes(order='C')
+        result = self.baiduclient.basicGeneral(content, self.options)
+        try:
+            return "".join([i['words'] for i in result['words_result']])
+        except:
+            return ''
+
+    def write_to_csv(self):
+        cropped = self.crop_image()
+        container = np.zeros(self.table_shape, dtype=object)
+        for col in range(self.table_shape[1]):
+            for row in range(self.table_shape[0]):
+                image = cropped[col*self.table_shape[0]+row]
+                words = self.image2words(image)
+                container[row, col] = words
+
+        df = pd.DataFrame(data=container)
+        #df.drop()
+        affix = self.image_dir.split('.')[-1]
+        csv_dir = self.image_dir.replace("."+affix, '.csv')
+        df = df.replace('',np.nan).dropna(axis=1,how='all').dropna(axis=0,how='all')
+        df.to_csv(csv_dir, index=None, header=False)
+        print(f'Success, write to file:{csv_dir}')
+
+
+
+if __name__ == '__main__':
+    pass
+    #img2csv = Image2Csv_CL('/Users/sfi/Desktop/ocr/test4.png')
+    #img2csv.write_to_csv()
+    #img2csv = Image2Csv_CL('/Users/sfi/Desktop/ocr/test5.png')
+    #img2csv.write_to_csv()
+    #img2csv = Image2Csv_CL('/Users/sfi/Desktop/ocr/test6.png')
+    #img2csv.write_to_csv()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
